ec454886cc7a062374202691e299fd646edcdd39
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-09-15 00:10:08 -0500

    added raycast field of view

---
+++ Assets/EnemyChaseView.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChaseView : MonoBehaviour
+{
+   
+    public Transform cast;
+    public Transform player;
+    public float agroRange;
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    public bool facingLeft;
+    private bool isAgro =false;
+    private bool isSearching =false;
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dir = new Vector3(player.position.x, player.position.y, 0);
+        if(CanSeePlayer(agroRange))
+        {
+            isAgro = true;
+            
+        }
+        else
+        {
+            if(isAgro)
+            {
+                //isSearching = true;
+                if(!isSearching)
+                {
+                    isSearching = true;
+                    Invoke("StopChasingPlayer", 3);
+                }
+                
+            }
+            
+        }
+        if(isAgro)
+        {
+            ChasePlayer();
+        }
+        
+    }
+    void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+            facingLeft = false;
+
+        }
+        else
+        {
+            //rb2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+            facingLeft = true;
+
+        }
+        direction = player.position - transform.position;
+        rb2d.AddRelativeForce(direction.normalized * moveSpeed, ForceMode2D.Force);
+
+    }
+    void StopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
+        isAgro = false;
+        isSearching=false;
+
+    }
+    bool CanSeePlayer(float distance)
+    {
+        bool val = false;
+        var castDist = distance;
+
+        if(facingLeft ==true)
+        {
+            castDist = -distance;
+        }
+
+        Vector2 endPos = cast.position + Vector3.right * castDist;
+        //Vector2 endPos = cast;
+        RaycastHit2D hit = Physics2D.Linecast(cast.position, endPos, 1 << LayerMask.NameToLayer("Action"));
+        if(hit.collider !=null)
+        {
+            if(hit.collider.gameObject.CompareTag("Player"))
+            {
+                val = true;
+            }
+            else
+            {
+                val = false;
+            }
+            Debug.DrawLine(cast.position, hit.point, Color.green);
+
+        }
+        else
+        {
+            Debug.DrawLine(cast.position, endPos, Color.blue);
+        }
+        return val;
+
+
+    }
+}
---
+++ Assets/EnemyFieldOfView.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFieldOfView : MonoBehaviour
+{
+    public float radius = 5f;
+    [Range(1,360)]public float angle =45f;
+    public LayerMask targetLayer;
+    public LayerMask obstructionLayer;
+    public GameObject playerRef;
+    public bool CanSeePlayer {get; private set;}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVCheck());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator FOVCheck()
+    {
+        WaitForSeconds wait =new WaitForSeconds(0.2f);
+        while(true)
+        {
+            yield return wait;
+            FOV();
+        }
+    }
+    private void FOV()
+    {
+        Collider2D[] rangeCheck =Physics2D.OverlapCircleAll(transform.position,radius,targetLayer);
+        if(rangeCheck.Length>0)
+        {
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget=(target.position-transform.position).normalized;
+            if(Vector2.Angle(transform.up,directionToTarget)<angle/2)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position,target.position);
+
+                if(!Physics2D.Raycast(transform.position,directionToTarget,distanceToTarget,obstructionLayer))
+                CanSeePlayer=true;
+                else
+                CanSeePlayer=false;
+            }
+            else
+            CanSeePlayer= false;
+
+            }
+            else if (CanSeePlayer)
+            CanSeePlayer=false;
+        }
+        private void OnDrawGizmos() {
+            
+                Gizmos.color=Color.white;
+                UnityEditor.Handles.DrawWireDisc(transform.position,Vector3.forward,radius);
+                Vector3 angle01 = DirectionFromAngle(-transform.eulerAngles.z,-angle/2);
+                Vector3 angle02 = DirectionFromAngle(transform.eulerAngles.z,angle/2);
+                Gizmos.color =Color.yellow;
+                Gizmos.DrawLine(transform.position,transform.position + angle01 * radius);
+                Gizmos.DrawLine(transform.position,transform.position + angle02 * radius);
+
+                if(CanSeePlayer)
+                {
+                    Gizmos.color =Color.green;
+                    Gizmos.DrawLine(transform.position,playerRef.transform.position);
+                }
+
+            
+        }
+        private Vector2 DirectionFromAngle(float eulerY, float angleInDegrees)
+        {
+            angleInDegrees += eulerY;
+            return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
+
+    }
+
