3e0629a0d41e98f207c8cfc46e1f22764437c9a3
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-09-27 09:40:05 -0500

    Add speed enum for controller

--- Assets/Scripts/PlayerController2.0/MovementV2.cs
+++ Assets/Scripts/PlayerController2.0/MovementV2.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerMovementState {
+    Sneaking,
+    Walking,
+    Running
+}
+
 public class MovementV2 : MonoBehaviour {
 
     [Header("Components")]
     private Rigidbody2D rb2d;
+    [SerializeField] private PlayerMovementState movementState = PlayerMovementState.Walking;
 
     [Header("Movement Variables")]
     [SerializeField] private float movementAcceleration;
@@ -25,26 +32,40 @@ public class MovementV2 : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        MoveCharacterHorizontal();
-        rb2d.drag = linearDrag;
+        MoveCharacter();
+        // rb2d.drag = linearDrag;
 
     }
 
-    private Vector2 GetInput() {
+    // Get input controllers
+    private static Vector2 GetInput() {
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-    private void MoveCharacterHorizontal() {
+    private void MoveCharacter() {
         rb2d.AddForce(new Vector2(horizontalDirection, verticalDirection) * movementAcceleration);
 
+        float currentMaxMovementSpeed = 0F;
+        switch (movementState) {
+            case PlayerMovementState.Sneaking:
+                currentMaxMovementSpeed = maxMovementSpeed / 2;
+                break;
+            case PlayerMovementState.Walking:
+                currentMaxMovementSpeed = maxMovementSpeed;
+                break;
+            case PlayerMovementState.Running:
+                currentMaxMovementSpeed = maxMovementSpeed * 2;
+                break;
+        }
+
         // Max Speed on X Axis
-        if (Mathf.Abs(rb2d.velocity.x) > maxMovementSpeed) {
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxMovementSpeed, rb2d.velocity.y);
+        if (Mathf.Abs(rb2d.velocity.x) > currentMaxMovementSpeed) {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * currentMaxMovementSpeed, rb2d.velocity.y);
         }
 
         // Max Speed on Y Axis
-        if (Mathf.Abs(rb2d.velocity.y) > maxMovementSpeed) {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign(rb2d.velocity.y) * maxMovementSpeed);
+        if (Mathf.Abs(rb2d.velocity.y) > currentMaxMovementSpeed) {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign(rb2d.velocity.y) * currentMaxMovementSpeed);
         }
     }
 
