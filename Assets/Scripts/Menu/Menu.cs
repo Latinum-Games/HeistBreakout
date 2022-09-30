0646348aff103c6b73a3cd856d689752da9cbea1
cgmKun <58601922+CGM-Charlie@users.noreply.github.com>
2022-09-29 23:34:32 -0500

    Squashed commit of the following:
    commit 8b7f8f9440ecd2f29cbb2997ee82eed8a26ba6a2
    Author: OmarSorchini <omarsorchinijr@gmail.com>
    Date:   Thu Sep 29 21:25:35 2022 -0500
    
        Menu implementations

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,13 @@
+public enum Menu
+{
+    MAIN_MENU,
+    OPTIONS_MENU,
+    EXIT_MENU,
+    PAUSE_MENU,
+    SHOP_MENU,
+    REWARD_MENU,
+    GAME_OVER_MENU,
+    LOBBY_MENU,
+    MUSEUM_MENU
+
+}
