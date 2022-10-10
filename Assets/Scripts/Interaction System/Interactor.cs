fd33cf001ac58c39c191fbe0a050f9cc0a66ff2d
cgmKun <58601922+CGM-Charlie@users.noreply.github.com>
2022-10-10 00:06:24 -0500

    Add Physics2D overlap to player area

---
+++ Assets/Scripts/Interaction System/Interactor.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour {
+
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 0.5f;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider2D[] _colliders = new Collider2D[3];
+    [SerializeField] private int _numFound;
+
+    private void Update() {
+        _numFound = Physics2D.OverlapCircleNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders,
+             _interactableMask);
+    }
+
+    private void OnDrawGizmos() {
+         Gizmos.color = Color.red;
+         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+}
