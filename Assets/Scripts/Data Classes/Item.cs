3e0f18f2c66e283a3205276fb0f6cecf5be7e67d
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-10-19 12:01:26 -0500

    Import Treasure Icons to Resources

--- Assets/Scripts/Data Classes/Item.cs
+++ Assets/Scripts/Data Classes/Item.cs
@@ -12,7 +12,23 @@ public class Item {
 
     // Specific Item Classification
     public enum ItemSprite {
-        Duck
+        Ring,
+        Harp,
+        Staff,
+        Necklace,
+        Goblet,
+        Crown,
+        Diamond,
+        Emerald,
+        Egg,
+        Jar,
+        FaceMask,
+        Mask,
+        Globe,
+        Duck,
+        Clock,
+        Ruby,
+        Sapphire
     }
 
     // Attributes
@@ -28,7 +44,24 @@ public class Item {
     public Sprite GetSprite() {
         switch (itemSprite) {
             default:
-                case ItemSprite.Duck: return ItemAssets.Instance.duck;
+            // Treasure Sprites
+            case ItemSprite.Ring: return ItemAssets.Instance.ring;
+            case ItemSprite.Harp: return ItemAssets.Instance.harp;
+            case ItemSprite.Staff: return ItemAssets.Instance.staff;
+            case ItemSprite.Necklace: return ItemAssets.Instance.necklace;
+            case ItemSprite.Goblet: return ItemAssets.Instance.goblet;
+            case ItemSprite.Crown: return ItemAssets.Instance.crown;
+            case ItemSprite.Diamond: return ItemAssets.Instance.diamond;
+            case ItemSprite.Emerald: return ItemAssets.Instance.emerald;
+            case ItemSprite.Egg: return ItemAssets.Instance.egg;
+            case ItemSprite.Jar: return ItemAssets.Instance.jar;
+            case ItemSprite.FaceMask: return ItemAssets.Instance.faceMask;
+            case ItemSprite.Mask: return ItemAssets.Instance.mask;
+            case ItemSprite.Globe: return ItemAssets.Instance.globe;
+            case ItemSprite.Duck: return ItemAssets.Instance.duck;
+            case ItemSprite.Clock: return ItemAssets.Instance.clock;
+            case ItemSprite.Ruby: return ItemAssets.Instance.ruby;
+            case ItemSprite.Sapphire: return ItemAssets.Instance.sapphire;
         }
     }
 }
--- Assets/Scripts/Data Classes/ItemAssets.cs
+++ Assets/Scripts/Data Classes/ItemAssets.cs
@@ -5,7 +5,23 @@ public class ItemAssets : MonoBehaviour {
     
     // Singleton to load assets from system files
     public static ItemAssets Instance { get; private set; }
+    public Sprite ring = null;
+    public Sprite harp = null;
+    public Sprite staff = null;
+    public Sprite necklace = null;
+    public Sprite goblet = null;
+    public Sprite crown = null;
+    public Sprite diamond = null;
+    public Sprite emerald = null;
+    public Sprite egg = null;
+    public Sprite jar = null;
+    public Sprite faceMask = null;
+    public Sprite mask = null;
+    public Sprite globe = null;
     public Sprite duck = null;
+    public Sprite clock = null;
+    public Sprite ruby = null;
+    public Sprite sapphire = null;
 
     private void Awake() {
         Instance = this;
@@ -13,6 +29,22 @@ public class ItemAssets : MonoBehaviour {
 
     public void Start() {
         // Load all assets from resources
-        duck = Resources.Load<Sprite>("Images/RubberDuck");
+        ring = Resources.Load<Sprite>("Sprites/Items/Anillo");
+        harp = Resources.Load<Sprite>("Sprites/Items/Arpa");
+        staff = Resources.Load<Sprite>("Sprites/Items/Cetro");
+        necklace = Resources.Load<Sprite>("Sprites/Items/Collar");
+        goblet = Resources.Load<Sprite>("Sprites/Items/Copa");
+        crown = Resources.Load<Sprite>("Sprites/Items/Corona");
+        diamond = Resources.Load<Sprite>("Sprites/Items/Diamante");
+        emerald = Resources.Load<Sprite>("Sprites/Items/Esmeralda");
+        egg = Resources.Load<Sprite>("Sprites/Items/Huevo");
+        jar = Resources.Load<Sprite>("Sprites/Items/Jarron");
+        faceMask = Resources.Load<Sprite>("Sprites/Items/Mascara");
+        mask = Resources.Load<Sprite>("Sprites/Items/Mascarilla");
+        globe = Resources.Load<Sprite>("Sprites/Items/Mundo");
+        duck = Resources.Load<Sprite>("Sprites/Items/Pato");
+        clock = Resources.Load<Sprite>("Sprites/Items/Reloj");
+        ruby = Resources.Load<Sprite>("Sprites/Items/Ruby");
+        sapphire = Resources.Load<Sprite>("Sprites/Items/Zafiro");
     }
 }
