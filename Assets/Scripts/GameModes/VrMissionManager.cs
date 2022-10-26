61fc056e3e567674b4f1a608a10f613889a5f0ca
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-10-26 12:04:08 -0500

    Create VR mission Game Mode Manager and UI

--- Assets/Scripts/GameModes/VrMissionManager.cs
+++ Assets/Scripts/GameModes/VrMissionManager.cs
@@ -1,17 +1,136 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class VrMissionManager : MonoBehaviour {
     
-    // Start is called before the first frame update
-    void Start() {
+    // Foreign Game Systems
+    [Header("Foreign Managers")] 
+    [SerializeField] private InputController inputController;
+    
+    // Brief Mission Variables
+    [Header("Mission Brief Data")] 
+    [SerializeField] private GameObject missionBriefModal;
+    [SerializeField] private GameObject startMissionButton;
 
+    // Stopwatch Variables
+    [Header("Stopwatch")] 
+    public float currentTime;
+    [SerializeField] private int startMinutes;
+    [SerializeField] private int startSeconds;
+    [SerializeField] private GameObject timerLabel;
+    private TextMeshProUGUI timerLabelText;
+    [SerializeField] private bool isTimerActive;
+
+    // Score and Objectives Variables
+    [Header("Score and Objectives")] 
+    [SerializeField] private int score;
+    [SerializeField] private int scoreMultiplier;
+
+    // Countdown To Start Variables
+    [Header("Countdown to Start")] 
+    [SerializeField] private int countdownTimer;
+    [SerializeField] private GameObject countdownLabel;
+
+    private void Awake() {
+        // Subscribe partial controller to just enable UI controls and not pause menu controls
+        inputController.PartialSubscribeMenuMap();
+        currentTime = (startMinutes * 60) + startSeconds;
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void Start() {
+        // Set Timer Text Mesh Pro Component
+        timerLabelText = timerLabel.GetComponentInChildren<TextMeshProUGUI>();
         
+        // Add listener to button
+        startMissionButton.GetComponent<Button>().onClick.AddListener(StartVRMission);
+        
+        // Set modal to activate
+        StartCoroutine(InitialCountdown());
+    }
+
+    private void Update() {
+        UpdateTimer();
+    }
+    
+    // Mission Brief Modal Functions
+    private void StartVRMission() {
+        // Close Modal Animaation
+        LeanTween.cancel(missionBriefModal);
+        LeanTween.scale(missionBriefModal, new Vector3(0f, 0f, 0f), 0.6f).setEaseOutBounce().setOnComplete(() => {
+            
+            // Start game loop
+            StartCoroutine(CountdownToStart());
+        });
+    }
+
+    // Timer Functions
+    private void UpdateTimer() {
+        if (isTimerActive) {
+            score = Mathf.RoundToInt((currentTime * 100) * scoreMultiplier);
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0) {
+                StopTimer(); 
+                
+                // TODO: LOSE CONDITION + REASON?
+                
+            }
+        }
+
+        var time = TimeSpan.FromSeconds(currentTime);
+        timerLabelText.text = time.ToString(@"mm\:ss\:ff");
+    }
+    
+    private void StartTimer() {
+        // Start Timer Animation
+        LeanTween.cancel(timerLabel);
+        var timerLabelRt = timerLabel.GetComponent <RectTransform>();
+        LeanTween.move(timerLabel, timerLabelRt.position - new Vector3(0, 70, 0), 0.5f).setEaseInOutExpo();
+
+        isTimerActive = true;
+    }
+
+    private void StopTimer() {
+        isTimerActive = false;
+    }
+    
+    // Coroutines
+    IEnumerator InitialCountdown() {
+        yield return new WaitForSeconds(0.7f);
+        
+        // Open Mission Brief Modal
+        LeanTween.cancel(missionBriefModal);
+        LeanTween.scale(missionBriefModal, new Vector3(1f, 1f, 1f), 0.5f).setEaseOutElastic();
+    }
+    
+    IEnumerator CountdownToStart() {
+        // Open Countdown Timer Animation
+        LeanTween.cancel(countdownLabel);
+        LeanTween.scale(countdownLabel, new Vector3(1f, 1f, 1f), 0.5f).setEaseOutElastic();
+        
+        var label = countdownLabel.GetComponentInChildren<TextMeshProUGUI>();
+        
+        while (countdownTimer > 0) {
+            label.text = countdownTimer.ToString();
+            yield return new WaitForSeconds(0.5f);
+            countdownTimer--;
+        }
+
+        label.text = "GO!";
+        
+        // TODO: START GAME LOOP
+        
+        // Update Controller Maps
+        inputController.DisableMenuMap();
+        inputController.SubscribePlayerOverworldMap();
+
+        // Close Countdown Timer Animation
+        LeanTween.cancel(countdownLabel);
+        LeanTween.scale(countdownLabel, new Vector3(0f, 0f, 0f), 0.6f).setEaseOutBounce().setOnComplete(() => {
+            StartTimer();
+            countdownLabel.SetActive(false); 
+        });
     }
 }
