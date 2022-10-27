9ae5521ab0ba513bcf17b34e2ac9af3e0cfc64c8
Carlos Mora <carlos.g.mora.md@gmail.com>
2022-10-27 09:01:09 -0500

    Fix character stucking to wall

--- Assets/Scripts/UI/Inventory/InventoryUI.cs
+++ Assets/Scripts/UI/Inventory/InventoryUI.cs
@@ -29,52 +29,56 @@ public class InventoryUI : MonoBehaviour {
     }
     
     private void RefreshInventoryItems() {
-        foreach (Transform child in itemSlotContainer) {
-            if (child == itemSlotTemplate) continue;
-            Destroy(child.gameObject);
-        }
-        
-        // Initial Coordinates Relative to Container
-        var x = 0;
-        var y = 0;
-        
-        // Size of Item Frame
-        var itemSlotCellSize = 100f;
-        var offsetX = 20f;
-        var offsetY = 20f;
+        if (itemSlotContainer != null) {
 
-        foreach (var item in inventory.GetItemList()) {
-            var itemSlotRectTransform =
-                Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
-            
-            // Instantiate Element in UI
-            // TODO: UPDATE VIEW TO AN SCROLL VIEW
-            itemSlotRectTransform.gameObject.SetActive(true);
-            itemSlotRectTransform.anchoredPosition = new Vector2((x * itemSlotCellSize) + offsetX, (y * -itemSlotCellSize) - offsetY);
-            
-            // Set Sprite
-            var sprite = item.GetSprite();
-            itemSlotRectTransform.GetComponent<Image>().sprite = sprite;
-            
-            // Set Amount Label
-            var quantityLabel = itemSlotRectTransform.Find("QuantityLabel").GetComponent<TextMeshProUGUI>();
-            quantityLabel.SetText(item.amount > 1 ? item.amount.ToString() : "");
+            foreach (Transform child in itemSlotContainer) {
+                if (child == itemSlotTemplate) continue;
+                Destroy(child.gameObject);
+            }
 
-            // On Select Animations
-            itemSlotRectTransform.GetComponent<ButtonEventHandlers>().onSelectAction.AddListener(delegate {
-                OnSelectButton(itemSlotRectTransform.gameObject);
-            });
-            
-            itemSlotRectTransform.GetComponent<ButtonEventHandlers>().onDeselectAction.AddListener(delegate {
-                OnDeselectButton(itemSlotRectTransform.gameObject);
-            });
+            // Initial Coordinates Relative to Container
+            var x = 0;
+            var y = 0;
 
-            x++;
+            // Size of Item Frame
+            var itemSlotCellSize = 100f;
+            var offsetX = 20f;
+            var offsetY = 20f;
 
-            // Row delimiter
-            if (x >= 5) {
-                x = 0;
-                y++;
+            foreach (var item in inventory.GetItemList()) {
+                var itemSlotRectTransform =
+                    Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+
+                // Instantiate Element in UI
+                // TODO: UPDATE VIEW TO AN SCROLL VIEW
+                itemSlotRectTransform.gameObject.SetActive(true);
+                itemSlotRectTransform.anchoredPosition = new Vector2((x * itemSlotCellSize) + offsetX,
+                    (y * -itemSlotCellSize) - offsetY);
+
+                // Set Sprite
+                var sprite = item.GetSprite();
+                itemSlotRectTransform.GetComponent<Image>().sprite = sprite;
+
+                // Set Amount Label
+                var quantityLabel = itemSlotRectTransform.Find("QuantityLabel").GetComponent<TextMeshProUGUI>();
+                quantityLabel.SetText(item.amount > 1 ? item.amount.ToString() : "");
+
+                // On Select Animations
+                itemSlotRectTransform.GetComponent<ButtonEventHandlers>().onSelectAction.AddListener(delegate {
+                    OnSelectButton(itemSlotRectTransform.gameObject);
+                });
+
+                itemSlotRectTransform.GetComponent<ButtonEventHandlers>().onDeselectAction.AddListener(delegate {
+                    OnDeselectButton(itemSlotRectTransform.gameObject);
+                });
+
+                x++;
+
+                // Row delimiter
+                if (x >= 5) {
+                    x = 0;
+                    y++;
+                }
             }
         }
     }
