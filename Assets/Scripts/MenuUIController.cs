c3f2291ac4c96cb8e3d0fc5b10f750fd4dbef8cf
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-11-10 18:43:40 -0600

    Add Initial Multiplayer Pipeline (#19)
    * Multiplayer updated
    
    * multiplayer updated
    
    * fix playercontroller controller
    
    Co-authored-by: Jared Flores <90533617+JaredFG@users.noreply.github.com>

---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MenuUIController : MonoBehaviourPunCallbacks
+{
+    public GameObject mainWindow;//Objeto que contiene ventana principal
+    public GameObject lobbyWidow; // Objeto que contiene lobby
+
+    [Header("Menu Principal")]
+    public Button createRoomBtn; // Boton de crear room
+    public Button joinRoomBtn; //Boton de unirse a room
+
+    [Header("Lobby")]
+    public Button startGameBtn; // Boton de iniciar partida
+    public TextMeshProUGUI playertextList; //Texto para imprimir jugadores en room
+
+
+    /// <summary>
+    /// Verificar si se establecio la conexion
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        createRoomBtn.interactable = true; // Activar boton de crear room
+        joinRoomBtn.interactable = true; //Activar botton de unirse a room
+    }
+    /// <summary>
+    /// Obtener nickname de jugador desde input
+    /// </summary>
+    /// <param name="_playerName"> input donde se puede editar el nombre</param>
+    public void GetPlayerName(TMP_InputField _playerName)
+    {
+        PhotonNetwork.NickName = _playerName.text; // Asignar el nombre de player en photon
+    }
+
+    /// <summary>
+    /// Permite unirse a un room Existente
+    /// </summary>
+    /// <param name="_roomName">input donde se lee el nombre de room </param>
+    public void JoinRoom(TMP_InputField _roomName)
+    {
+        _NetworkManager.instance.JoinRoom(_roomName.text);//Conectar a manager de conexion y enviar nombre de room
+    }
+
+    /// <summary>
+    /// Crear un nuevo room
+    /// </summary>
+    /// <param name="_roomName"> Input de nombre que va recibir el room</param>
+    public void CreateRoom(TMP_InputField _roomName)
+    {
+        _NetworkManager.instance.CreateRoom(_roomName.text); //Crear nuevo room desde networkManager
+    }
+
+    /// <summary>
+    /// Verificar si ya se unio el player a un room
+    /// </summary>
+    public override void OnJoinedRoom()
+    {
+        lobbyWidow.SetActive(true);
+        mainWindow.SetActive(false);
+        photonView.RPC("UpdatePlayerInfo", RpcTarget.All);
+    }
+    /// <summary>
+    /// Actualizar la informacion dentro de lobby 
+    /// </summary>
+    [PunRPC]
+    public void UpdatePlayerInfo()
+    {
+        playertextList.text = ""; //Limpiar campo de texto
+        foreach (Player player in PhotonNetwork.PlayerList) //Ciclo para pintar todos los player en el Room
+        {
+            playertextList.text += player.NickName + "\n";//Agregar nombre de players
+        }
+
+
+        if (PhotonNetwork.IsMasterClient) //Verificar si el cliente es Master Clien
+        {
+            startGameBtn.interactable = true;//Activar boton de iniciar juego
+        }
+        else
+        {
+            startGameBtn.interactable = false; // Desactivar boton de inicio de juego
+        }
+    }
+
+    /// <summary>
+    /// Salir del room actual
+    /// </summary>
+    public void LeaveLobby()
+    {
+        lobbyWidow.SetActive(false); //Ocultar ventant de lobby
+        mainWindow.SetActive(true);//Activar ventana de main
+
+        _NetworkManager.instance.LeveaRoom();// ejecurtar funcion de salir de room
+        UpdatePlayerInfo();//Actualizar la informacion
+    }
+
+    /// <summary>
+    /// Abrir nueva escena  con el juego
+    /// </summary>
+    public void StartGame()
+    {
+        _NetworkManager.instance.photonView.RPC("LoadScene", RpcTarget.All, "SceneGamePlay");
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    // FIELD OF VIEW
+    [SerializeField] private FieldOfView fieldOfView;
+
+    [SerializeField] private float playerSpeed = 2.0f;
+    //[SerializeField]
+    //private float inputSmoothDamp  = .3f;
+    [SerializeField] private float smoothInputSpeed = .2f;
+
+    private PlayerInput playerInput;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+
+    private InputAction moveAction;
+    private InputAction velocityAction;
+    private InputAction hitAction;
+
+    private Vector2 currentInputVector;
+    private Vector2 smoothInputVelocity;
+    private float vecX;
+    private float vecY;
+    public float mag;
+
+    [SerializeField] public float maxSoundArea = 2;
+
+    public enum Velocity { Slow, Normal, Fast }
+    public Velocity velocity;
+    private int velState = 1;
+    [SerializeField] private bool isChangingVel = true;
+    [SerializeField] private bool isHitting = false;
+
+    public GameObject HitBox;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        velocityAction = playerInput.actions["Velocity"];
+        hitAction = playerInput.actions["Hit"];
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (velState == 0)
+        {
+            velocity = Velocity.Slow;
+            maxSoundArea = 2f;
+            playerSpeed = 4f;
+
+        }
+        if (velState == 1)
+        {
+            velocity = Velocity.Normal;
+            maxSoundArea = 5f;
+            playerSpeed = 5f;
+
+        }
+        if (velState == 2)
+        {
+            velocity = Velocity.Fast;
+            maxSoundArea = 7f;
+            playerSpeed = 7f;
+
+        }
+        if (velocityAction.IsPressed() && isChangingVel)
+        {
+            StartCoroutine(changeVel());
+
+        }
+        if (hitAction.IsPressed() && !isHitting)
+        {
+            StartCoroutine(actHitBox());
+
+        }
+        //Debug.Log(velocityAction.IsPressed());
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        currentInputVector = Vector2.SmoothDamp(currentInputVector, input, ref smoothInputVelocity, smoothInputSpeed);
+        Vector3 move = new Vector3(currentInputVector.x, currentInputVector.y, 0);
+        // vecX = move.x;
+        // vecY = move.y;
+        mag = move.magnitude;
+
+        mag = Mathf.Clamp(mag, 0.1f, 1.0f);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+        fieldOfView.SetOrigin(transform.position);
+
+        //Debug.Log(transform.position);
+        //Debug.Log(mag);
+    }
+
+    IEnumerator changeVel()
+    {
+        isChangingVel = false;
+
+        if (velState == 2)
+        {
+            velState = 0;
+        }
+        else
+        {
+            velState++;
+        }
+
+        yield return new WaitForSeconds(1);
+        isChangingVel = true;
+    }
+    IEnumerator actHitBox()
+    {
+        isHitting = true;
+        HitBox.SetActive(true);
+        yield return new WaitForSeconds(1);
+        HitBox.SetActive(false);
+        isHitting = false;
+    }
+}
